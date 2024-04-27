0c20bf1b412f25b3f05c652b376a6b1a473cb1a3
Ihor <kovaluk_igor@ukr.net>
2024-04-27 15:02:38 +0200

    Update Card.jsx

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
 import './card.scss'
 import pin from '../../icons/pin.png'
+import bed from '../../icons/bed.png'
+import bath from '../../icons/bath.png'
+import save from '../../icons/save.png'
+import chat from '../../icons/chat.png'
 
 function Card({item}) {
     return (
@@ -18,7 +22,24 @@ function Card({item}) {
                 </p>
                 <p className='price'>$ {item.price}</p>
                 <div className='bottom'>
-                    
+                    <div className="features">
+                        <div className="feature">
+                            <img src={bed} alt="" />
+                            <span>{item.bedroom } bedroom</span>
+                        </div>
+                        <div className="feature">
+                            <img src={bath} alt="" />
+                            <span>{item.bathroom }bathroom</span>
+                        </div>
+                    </div>
+                    <div className="icons">
+                        <div className="icon">
+                            <img src={save} alt="" />
+                        </div>
+                        <div className="icon">
+                            <img src={chat} alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
