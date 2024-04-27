c42e24d84b484fb5c2b9f59cac80b5acac86ce6d
Ihor <kovaluk_igor@ukr.net>
2024-04-27 22:22:26 +0200

    Update Map.jsx

--- src/routes/listPage/map/Map.jsx
+++ src/routes/listPage/map/Map.jsx
@@ -10,21 +10,7 @@ function Map({ items }) {
 
     return (
 
-//  <MapContainer center={[50.5074, -0.1278]} zoom={7} scrollWheelZoom={false} className='map'>
-//       <TileLayer
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       />
-            
-//       <Marker position={[51.5074, -0.1278]}>
-//         <Popup>
-//           A pretty CSS3 popup. <br /> Easily customizable.
-//         </Popup>
-//       </Marker>
-//     </MapContainer>
-
-
-    <MapContainer center={[52.505, -1.8]} zoom={7} scrollWheelZoom={false} className='map'>
+    <MapContainer center={[51.505, -1.8]} zoom={7} scrollWheelZoom={false} className='map'>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
